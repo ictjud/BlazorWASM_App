0a658e46e1d78ade1ae588700f245a21c0dba6ca
Jud ICT <ictjud@outlook.com>
2025-03-20 15:59:22 +0000

    Creating services, starting the category service.

---
+++ ClientLibrary/Services/CategoryService.cs
@@ -0,0 +1,41 @@
+﻿using ClientLibrary.Helper;
+using ClientLibrary.Models;
+using ClientLibrary.Models.Category;
+using ClientLibrary.Models.Product;
+
+namespace ClientLibrary.Services
+{
+    public class CategoryService(IHttpClientHelper httpClientHelper, IApiCallHelper apiCallHelper)
+        : ICategoryService
+    {
+        public Task<ServiceResponse> AddCategoryAsync(CreateCategory category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ServiceResponse> DeleteCategoryAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<GetCategory>> GetAllCategoryAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<GetCategory> GetCategoryByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<GetProduct>> GetProductsByCategoryAsync(Guid categoryId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ServiceResponse> UpdateCategoryAsync(UpdateCategory category)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ClientLibrary/Services/ICategoryService.cs
@@ -0,0 +1,16 @@
+﻿using ClientLibrary.Models;
+using ClientLibrary.Models.Category;
+using ClientLibrary.Models.Product;
+
+namespace ClientLibrary.Services
+{
+    public interface ICategoryService
+    {
+        Task<IEnumerable<GetCategory>> GetAllCategoryAsync();
+        Task<GetCategory> GetCategoryByIdAsync(int id);
+        Task<ServiceResponse> AddCategoryAsync(CreateCategory category);
+        Task<ServiceResponse> UpdateCategoryAsync(UpdateCategory category);
+        Task<ServiceResponse> DeleteCategoryAsync(int id);
+        Task<IEnumerable<GetProduct>> GetProductsByCategoryAsync(Guid categoryId);
+    }
+}
